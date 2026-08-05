43126d9fafd4077847ad679b1e33c9644da1be45
DENIZ EFE ERSUMER <denizefe.ersumer@gmail.com>
2026-08-05 14:44:21 +0300

    ata

--- obj/Debug/net10.0/turkiye_haritası.AssemblyInfo.cs
+++ obj/Debug/net10.0/turkiye_haritası.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("turkiye_haritası")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6acaf907ffeef399dca7eaa8df6d3e5fa07d09c")]
 [assembly: System.Reflection.AssemblyProductAttribute("turkiye_haritası")]
 [assembly: System.Reflection.AssemblyTitleAttribute("turkiye_haritası")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
